7b07132df644141431c9a9c1d7ee447d3bbec096
root <root@raspi-meterkast.home.fazant.net>
2016-10-15 23:44:41 +0200

    Changed sheang in setup.py to use python3

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 from distutils.core import setup
 
@@ -9,18 +9,21 @@ setup(  name='iskra-me162',
         version='0.1.0',
         description="Script that feeds ME162 readings as a P1 meter to Domoticz",
         license="GPLv3",
-        long_description=read("README.rst"),
+        # long_description=read("README.rst"),
         author='Louis Lagendijk',
         author_email='louis.lagendijk@gmail.com',
-        scripts = ["iskra-me162"],
-	data_files = [("/etc/default", ["iskra-me162.config"]),
-		      ("/etc/systemd/system", ["iskra-me162.service"]),
-		     ],
-	url = "https://github.com/llagendijk/iskra-me162.git",
+		url = "https://github.com/llagendijk/iskra-me162.git",
         classifiers = [
-                "Environment :: No Input/Output (Daemon)",
-                "Development Status :: 4 - Beta",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        ],
+                "Environment :: No Input/Output (Daemon)",
+                "Development Status :: 4 - Beta", 
+		],
+        scripts = ["iskra-me162"],
+		data_files = [
+				("/etc/default", ["iskra-me162.config"]),
+		      		("/etc/systemd/system", ["iskra-me162.service"]),
+		     ],
 )
 
